705d9b619f26e42e20b9269f0d9fc1e682d1cf6d
alexarevalo <aarevalo@snap.com>
2018-03-11 17:06:11 -0700

    Have a working game for the first value of my tvShows

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,111 @@
-// Creates an array that lists out all of the options (Shows in the hangman game).
-var tvShows = ["narcos", "stranger things", "peaky blinders", "mindhunter"];
+script>
+
+
+    // Creates an array that lists out all of the options (Shows in the hangman game).
+    var tvShows = ["narcos", "stranger things", "peaky blinders", "mindhunter"];
     
-// array of user guesses from the event key of pressing the keyboard
-var guess = [];
+    // array of user guesses from the event key of pressing the keyboard
+    var guess = [];
+
+    
+    // Creating variables to hold the number of wins, losses, and ties. They start at 0.
+    var wins = 0;
+    var losses = 0;
+    var numbersLetterGuess = 12;
+    
+
+    // Pull the characters of the first show and put into theShow, we now have N A R C O S
+
+    var theShow = tvShows[0].split('');
+      console.log(theShow);
+
+    //Input this number of characters into the Current Word
+
+    var emptySpace = theShow.length
 
 
-// Creating variables to hold the number of wins, losses, and ties. They start at 0.
-var wins = 0;
-var losses = 0;
-var numbersLetterGuess = 12;
+    //Start game with key press / Guess counter is set to 12
+    document.querySelector(".guessRemaining").innerHTML = numbersLetterGuess;
 
+ //Defining how many _ are needed for the empty space
+    var emptySpaceLetters=[];
 
-// Pull the characters of the first show and put into theShow, we now have N A R C O S
+    for (var i = 0; i < emptySpace; i++){
+      var emptySpaceVal = "_";
+      emptySpaceLetters.push(emptySpaceVal)
+        }
+      console.log(emptySpaceLetters);
 
-var theShow = tvShows[0].split('');
-console.log(theShow);
+    document.querySelector(".theShow").innerHTML = (emptySpaceLetters);
 
-//Input this number of characters into the Current Word
+    //Start the game with a key press
+    document.onkeyup = function(event) {
 
-var emptySpace = theShow.length
+   
+    
 
+    /*
+    //Set current word as theShow
 
-//Start game with key press / Guess counter is set to 12
-document.querySelector(".guessRemaining").innerHTML = numbersLetterGuess;
+    document.querySelector(".theShow").innerHTML = theShow;
 
-//Defining how many _ are needed for the empty space
-var emptySpaceLetters=[];
+    */
+    
+    
+    //On a key press, we add the variable keyPress to the array of guess
+    var keyPress = event.key
+    console.log(guess.indexOf(keyPress))
+      //If the key press by the user is the same we do nothing
+    if (guess.indexOf(keyPress) == -1){
+      guess.push(keyPress);
+      //Decreasing number of letter guesses / Game Loser alert
+    var guessRemaining = numbersLetterGuess--
+      document.querySelector(".guessRemaining").innerHTML = numbersLetterGuess;
+        }
+      else{
+        
+      };
+      // This is showing that the keys are being registered and the array is being built
+      console.log(keyPress);
+      console.log(guess);
+      
+      // Setting array values to show on the letters guessed
+      document.querySelector(".lettersGuess").innerHTML = guess;
 
-for (var i = 0; i < emptySpace; i++){
-  var emptySpaceVal = "_";
-emptySpaceLetters.push(emptySpaceVal)
-}
-console.log(emptySpaceLetters);
+      //Compare the Letters guessed vs the current word using a loop
 
-document.querySelector(".theShow").innerHTML = (" " + emptySpaceLetters + " ");
+      for (var i = 0 ; i < guess.length ; i++){
+      if (theShow.indexOf(guess[i]) > -1) {
+        /*console.log(theShow.indexOf(guess[i]));*/
+        /*console.log(theShow.indexOf(guess[i]) > -1);*/
+        var correctGuess = theShow.indexOf(guess[i]);
+        /*console.log(correctGuess);*/
+        var fillLetter = theShow[correctGuess]
+        /*console.log(fillLetter);*/
+        // adding the correct guesses to an array at correct position
+        emptySpaceLetters.splice(correctGuess, 1 ,fillLetter);
+        /*console.log(emptySpaceLetters);*/
+        document.querySelector(".theShow").innerHTML = emptySpaceLetters;
+      }
+      else {
 
-//Start the game with a key press
-document.onkeyup = function(event) {
+      }
+    }
+      
 
+      if (guessRemaining == 1){
+      alert("GAME OVER LOSER");
+        }
+       
+        if (emptySpaceLetters >= theShow){
+          console.log(emptySpaceLetters = theShow)
+          alert("Trapping Like a Narco! (Migos Quote)")
+        }
+        
 
-//Decreasing number of letter guesses / Game Loser alert
-var guessRemaining = numbersLetterGuess--
-document.querySelector(".guessRemaining").innerHTML = numbersLetterGuess;
-
-/*
-//Set current word as theShow
-
-document.querySelector(".theShow").innerHTML = theShow;
-
-*/
-if (guessRemaining == 0){
-  alert("GAME OVER LOSER");
-}
-
-
-//On a key press, we add the variable keyPress to the array of guess
-  var keyPress = event.key
-  guess.push(keyPress);
-  // This is showing that the keys are being registered and the array is being built
-  console.log(keyPress);
-  console.log(guess);
-
-  
-  // Setting array values to show on the letters guessed
-  document.querySelector(".lettersGuess").innerHTML = guess;
-  //Compare the Letters guessed vs the current word using a loop
-
-  for (var i = 0 ; i < guess.length ; i++)
-  console.log(theShow.indexOf(guess[i]));
-  if 
-
-};
+    };
+          
+    
 
  
-     
-
-
-/*
-<script>
-     //insert for loop here with 
-function myFunction() {
-    var str = ["hello world", "test"];
-    var res = str[0].substring(-3);
-    //insert the characters into an array variable , use push command 
-var computerLetters = [res[i]]
-  
-//Need to add #of divs that coincide with the # of characters in the new array, probably another loops
-
-
-   //Insert into document
-    document.getElementById("demo").innerHTML = res[0];
-}
-//Then you need to 
-</script>
-*/
+    </script>
